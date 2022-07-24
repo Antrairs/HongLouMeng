2d7b8d5d91efaae4ef3b2c21cdebdbd6d5a31021
Antrairs <60560003+Antrairs@users.noreply.github.com>
2022-07-24 11:25:09 +0800

    Add files via upload

---
+++ json/chapter13.js
@@ -0,0 +1,169 @@
+const data = [
+    {
+        content: '猢狲散',
+        explain: '比喻有权势的人一旦失势，则其依附者随即散去',
+        pinyin: 'hú sūn sàn'
+    },
+    {
+        content: '祖茔',
+        explain: '坟墓，坟地',
+        pinyin: 'zǔ yíng'
+    },
+    {
+        content: '孤恓',
+        explain: '寂寞凄凉；悲伤。',
+        pinyin: 'gū xī'
+    },
+    {
+        content: '彼时贾代儒，代修，贾敕，贾效，贾敦，贾赦，贾政，贾琮，贾扁，贾珩，贾珖，贾琛，贾琼，贾璘，贾蔷，贾菖，贾菱，贾芸，贾芹，贾蓁，贾萍，贾藻，贾蘅，贾芬，贾芳，贾兰，贾菌，贾芝等都来了。',
+        common: '不愧是大家族 和报菜名有一拼'
+    },
+    {
+        content: '讣闻',
+        explain: '向亲友报告丧事的通知，多附有死者事略。也作讣文',
+        pinyin: 'fù wén'
+    },
+    {
+        content: '解冤洗业醮',
+        explain: '超度亡魂之用，以解除平生冤仇、孽债，让亡魂安入天国',
+        pinyin: 'jiě yuān xǐ yiè jiào'
+    },
+    {
+        content: '恣意',
+        explain: '任意；任性。',
+        pinyin: 'zì yì'
+    },
+    {
+        content: '玎珰',
+        explain: '形容金属、瓷器等清凉的撞击声',
+        pinyin: 'dīng dāng'
+    },
+    {
+        content: '檀麝',
+        explain: '麝香和檀香',
+        pinyin: 'shè tán'
+    },
+    {
+        content: '殓',
+        explain: '给尸体穿衣下棺',
+        pinyin: 'liàn'
+    },
+    {
+        content: '紊乱',
+        pinyin: 'wěn luàn'
+    },
+    {
+        content: '揖',
+        explain: '拱手行礼',
+        pinyin: 'yī'
+    },
+    {
+        content: '束带顶冠',
+        explain: '捆扎腰带，戴上冠冕。比喻做官'
+    },
+    {
+        content: '诗书旧族',
+        explain: '指有文明历史的民族，即汉族'
+    },
+    {
+        content: '烈火烹油',
+        explain: '气氛很热烈的样子。也形容更加热闹，更有排场'
+    },
+    {
+        content: '鲜花着锦',
+        explain: '意思是比喻好上加好'
+    },
+    {
+        content: '盛筵必散',
+        explain: '再盛大的筵席也要散场。比喻繁华欢乐总会消失，不能久存',
+        pinyin: 'shèng yán bì sàn'
+    },
+    {
+        content: '停灵七日',
+        common: '“头七”就是人死后的第七天，据说，人的鬼魂会在这一天夜里回到家中，算是回来看最后一眼，也叫“回魂夜”。'
+    },
+    {
+        content: '眷属',
+        explain: '家眷、亲属或夫妻'
+    },
+    {
+        content: '节度使',
+        common: '古代官名 品级很高 唐朝初建立 元朝时被彻底废弃'
+    },
+    {
+        content: '赫赫扬扬',
+        explain: '显赫的样子'
+    },
+    {
+        content: '无虞',
+        explain: '太平无事',
+        pinyin: 'wú yú'
+    },
+    {
+        content: '倘或',
+        explain: '假如；如果'
+    },
+    {
+        content: '家塾',
+        explain: '旧时请老师到家里来教授自己子弟的私塾',
+        pinyin: 'jiā shú'
+    },
+    {
+        content: '扶挽',
+        explain: '搀扶'
+    },
+    {
+        content: '黉门监',
+        explain: '国子监的生员 监生 其监生之名当亦由“恩荫”或“捐纳”而得',
+        pinyin: 'hóng mén jiàn',
+        common: '买来的名声'
+    },
+    {
+        content: '灵幡',
+        explain: '旧俗出殡时孝子打的幡；殡葬时所持之幡，上写有表明死者身分的文字',
+        pinyin: 'líng fān'
+    },
+    {
+        content: '诰命',
+        explain: '又称诰书，是皇帝任命当朝官员，授赠给官员家属的荣誉证书'
+    },
+    {
+        content: '诰授',
+        explain: '朝廷用诰命授予封号',
+        pinyin: 'gào shòu'
+    },
+    {
+        content: '喜不自禁',
+        explain: '抑制不住内心的喜悦。形容极其高兴'
+    },
+    {
+        content: '内眷',
+        explain: '女眷'
+    },
+    {
+        content: '藏之不迭',
+        explain: '害怕见到男人，要马上躲开'
+    },
+    {
+        content: '杀伐决断',
+        explain: '处事 作出决断的能力'
+    },
+    {
+        content: '那凤姐素日最喜揽事办，好卖弄才干',
+        common: '凤姐喜爱办事 权力渐渐流到她手中'
+    },
+    {
+        content: '裙钗',
+        explain: '指裙子与头钗都是妇女的衣饰，旧时借指妇女',
+        pinyin: 'qún chāi'
+    },
+    {
+        content: '此时贾珍恨不能代秦氏之死，这话如何肯听。',
+        common: '贾珍作为公公面对儿媳之死内心“恨不能代秦氏之死” 未免过激 据前面细节可看出其二人有私情'
+    },
+    {
+        content: '他也触柱而亡',
+        common: '看来瑞珠也知情'
+    }
+
+]
