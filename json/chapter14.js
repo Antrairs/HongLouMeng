a81bbdbbf48d34c699b800568966b09077ad308b
Antrairs <60560003+Antrairs@users.noreply.github.com>
2022-07-24 22:08:06 +0800

    Add files via upload

---
+++ json/chapter14.js
@@ -0,0 +1,208 @@
+const data = [
+    {
+        content: '蠲',
+        explain: '积存',
+        pinyin: 'juān'
+    },
+    {
+        content: '完竣',
+        explain: '工程、事务等的完成',
+        pinyin: 'wán jùn'
+    },
+    {
+        content: '糊裱',
+        explain: '用纸等糊窗户﹑墙壁﹑顶棚等',
+        pinyin: 'hú biǎo'
+    },
+    {
+        content: '传谕',
+        explain: '旧谓帝王或上司下达指示',
+        pinyin: 'chuán yù'
+    },
+    {
+        content: '抱愧',
+        explain: '心中有愧；负疚',
+        pinyin: 'bào kuì'
+    },
+    {
+        content: '迩',
+        explain: '近',
+        pinyin: 'ěr'
+    },
+    {
+        content: '妯娌',
+        explain: '兄弟的妻子的合称',
+        pinyin: 'zhóu li'
+    },
+    {
+        content: '灵柩',
+        explain: '死者已经入殓的棺材',
+        pinyin: 'líng jiù'
+    },
+    {
+        content: '冢孙',
+        explain: '嫡长孙',
+        pinyin: 'zhǒng sūn'
+    },
+    {
+        content: '告殂',
+        explain: '死亡',
+        pinyin: 'gào cú'
+    },
+    {
+        content: '荫生',
+        explain: '封建社会中凭借上代余荫取得的监生资格',
+        pinyin: 'yīn shēng'
+    },
+    {
+        content: '瞥',
+        explain: '很快地看一下',
+        pinyin: 'piē'
+    },
+    {
+        content: '痰盒掸帚',
+        pinyin: 'tán hé dǎn zhǒu'
+    },
+    {
+        content: '烈货',
+        explain: '意思是泼辣货 垢骂的言辞',
+        common: '侧面描写凤姐的泼辣'
+    },
+    {
+        content: '脸酸心硬',
+        explain: '指不留情面而又心肠狠毒'
+    },
+    {
+        content: '挂幔守灵',
+        explain: '为死者守夜'
+    },
+    {
+        content: '张惶愧惧',
+        explain: '惭愧恐惧',
+        pinyin: 'zhāng huáng kuì jù'
+    },
+    {
+        content: '兢兢业业',
+        explain: '做事谨慎、勤恳、踏实'
+    },
+    {
+        content: '举止舒徐',
+        explain: '从容不迫的样子'
+    },
+    {
+        content: '纸札',
+        explain: '纸做的冥器',
+        pinyin: 'zhǐ zhá',
+        common: '象征着对去世人在另一边的美好祝愿'
+    },
+    {
+        content: '卯正二刻',
+        common: '早六点三十分 十二地支与十二生肖一致: 子丑寅卯辰巳午未申酉戌亥 按照每天二十四小时，则每个时辰为两个小时。每天从二十三点开始计算，23—1点为子时；以此类推 每个时辰起点为初，如五点为卯初；每个时辰中点为正，卯正则为早六点。'
+    },
+    {
+        content: '络子',
+        explain: '结的一种用法，做成袋子的模样用来装东西',
+        pinyin: 'lào zi'
+    },
+    {
+        content: '登时放下脸来，喝命：“带出去，打二十板子！”',
+        common: '当时顶撞县令也才二十板子 由此可见凤姐心狠手辣'
+    },
+    {
+        content: '王法',
+        explain: '封建时代的法律，亦泛指法律'
+    },
+    {
+        content: '世袭制',
+        explain: '古代传承制度',
+        common: '名号、爵位以及财产等按照血统关系世代传承 体现了封建家族联系的紧密'
+    },
+    {
+        content: '觑',
+        pinyin: 'qù',
+        explain: '窥视'
+    },
+    {
+        content: '描赔',
+        explain: '照原样赔偿'
+    },
+    {
+        content: '凤姐坐了，放声大哭',
+        common: '说哭就哭 表演能力可见一斑'
+    },
+    {
+        content: '见凤姐出声，都忙忙接声嚎哭',
+        common: '这哪是出殡来了 这是演戏来了'
+    },
+    {
+        content: '回押',
+        explain: '收据 用钱的证明'
+    },
+    {
+        content: '蹙眉长叹',
+        explain: '皱著眉头，长声叹息',
+        pinyin: 'cù méi cháng tàn'
+    },
+    {
+        content: '诰命',
+        explain: '特指有封号的妇人'
+    },
+    {
+        content: '诰封',
+        explain: '封建王朝对官员及其先代、妻室授予爵位或称号',
+        pinyin: 'gào fēng'
+    },
+    {
+        content: '蒋子宁',
+        common: '子鼠'
+    },
+    {
+        content: '牛继宗',
+        common: '丑牛'
+    },
+    {
+        content: '柳彪',
+        common: '寅虎'
+    },
+    {
+        content: '柳芳',
+        common: '卯兔'
+    },
+    {
+        content: '陈瑞文',
+        common: '辰龙'
+    },
+    {
+        content: '史鼎',
+        common: '巳蛇'
+    },
+    {
+        content: '马尚',
+        common: '午马'
+    },
+    {
+        content: '卫若兰',
+        common: '未羊'
+    },
+    {
+        content: '侯孝康',
+        common: '申猴'
+    },
+    {
+        content: '游击谢鲸',
+        common: '酉鸡'
+    },
+    {
+        content: '戚建辉',
+        common: '戌狗'
+    },
+    {
+        content: '石光珠',
+        common: '亥猪'
+    },
+    {
+        content: '不可枚数',
+        explain: '多的数不过来',
+        common: '老曹起名够随便的 十二生肖对应人名'
+    }
+]
